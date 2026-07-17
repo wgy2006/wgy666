c1ad6c7dd06d7ea1b1b865f8c13424b5a9584622
S1mpleWind <ivonjiao@gmail.com>
2026-07-17 17:09:09 +0800

    changing url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   define: {
     // 服务器后端
-    //'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://10.119.4.70:8000'),
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://10.119.4.70:8000'),
 
     // 本地调试用：
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://localhost:8000'),
+    // 'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://localhost:8000'),
   },
 })
